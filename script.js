3efa1b1f1bb1a0883eedd3ea00d70661dcbcbe4b
Admin <admin@users-Air.broadband>
2021-01-19 13:15:00 +0000

    First commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    //Toogle nav
+    burger.addEventListener('click', () => {
+        //Toggle nav
+        nav.classList.toggle('nav-active');
+
+        //Animate links
+        navLinks.forEach((link, index) => {
+            console.log(index)
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = 'navLinkFade 0.5s ease forwards 0.5s';
+            }
+        });
+    });
+
+}
+
+navSlide();
